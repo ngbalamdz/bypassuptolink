36aea36947d4de4ae4a4f2eec6523b67554653c6
ngbalamdz <lamncb2000@gmail.com>
2026-03-06 23:35:53 +0700

    script.user.js

---
+++ script.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Bypass UptoLink
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Bypass UptoLink for cuttay user
+// @require      https://github.com/ngbalamdz/bypassuptolink/raw/refs/heads/main/source.js
+// @match        *://*/*
+// @author       LamdDepTrai
+// @grant        GM_xmlhttpRequest
+// @connect      uptolink.one
+// @connect      raw.githubusercontent.com
+// @connect      api.github.com
+// @connect      generativelanguage.googleapis.com
+// @connect      *
+// @run-at       document-end
+// @icon         https://cdn.discordapp.com/avatars/1001691686721835090/8c9770820eac0449217292ed718b3700.png
+// ==/UserScript==
